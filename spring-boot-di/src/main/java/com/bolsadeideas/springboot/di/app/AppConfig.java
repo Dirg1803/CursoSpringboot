a801ea871387f96ff10b10a20f49694e73d6b60c
Diego Rosales <diego.rosales@ibm.com>
2022-03-04 19:26:25 -0600

    Se subi la practica de la sesion 3 parte 2

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
@@ -1,13 +1,10 @@
 package com.bolsadeideas.springboot.di.app;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
 import com.bolsadeideas.springboot.di.app.models.domain.ItemFactura;
 import com.bolsadeideas.springboot.di.app.models.domain.Producto;
 import com.bolsadeideas.springboot.di.app.models.service.IServicio;
@@ -44,12 +41,16 @@ public class AppConfig {
 	
 	@Bean("itemsFacturaOficina")
 	public List<ItemFactura> registrarItemsoficina(){
-		Producto prducto1= new Producto("Monitor LG LCD 24", 100);
-		Producto prducto2= new Producto("Bicicleta Bianchi aro 26", 200);
+		Producto prducto1= new Producto("Monitor LG LCD 24", 250);
+		Producto prducto2= new Producto("Notebook Asus", 500);
+		Producto prducto3= new Producto("Impresora HP Multifuncional", 80);
+		Producto prducto4= new Producto("Escritorio Oficina", 300);
 		
 		ItemFactura linea1 = new ItemFactura(prducto1, 2);
-		ItemFactura linea2 = new ItemFactura(prducto2, 4);
+		ItemFactura linea2 = new ItemFactura(prducto2, 1);
+		ItemFactura linea3 = new ItemFactura(prducto3, 1);
+		ItemFactura linea4 = new ItemFactura(prducto4, 1);
 		
-		return Arrays.asList(linea1,linea2);
+		return Arrays.asList(linea1,linea2,linea3,linea4);
 	}
 }
